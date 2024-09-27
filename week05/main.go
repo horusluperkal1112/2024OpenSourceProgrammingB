575dc66bc8735a121c4f9faed491061a033eab00
horusluperkal1112 <sangminbag034@naver.com>
2024-09-27 18:42:44 +0900

    v0.3 IO $ Type

--- week05/main.go
+++ week05/main.go
@@ -1,13 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"reflect"
+	"strings"
+)
 
 func main() {
-	var i int
-	i = 55
+	// var i int
+	// i = 55
+	var f float64 = 1.92
+	//var i int = 55
+	//f := 1.92
+	i := "55"
+	//C언어 double : float64
+	//var f float32 = 1.92
+	fmt.Println(strings.Title("kim inha"))
+	fmt.Printf("%f %f\n", f, math.Ceil(f))
+	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(i))
+	fmt.Println("i is", f)
 	fmt.Println("i is", i)
-	fmt.Print("i is", i)
+	fmt.Print("i is", i, "\n")
 	fmt.Println("i is", i)
-	fmt.Printf("i i %d\ns", i)
+	//fmt.Printf("i i %d\n", i)
+	fmt.Printf("i i %s\n", i)
 
 }
