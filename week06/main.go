3d10b5fb671bf6b4a7d1554593ae11d25b099bea
horusluperkal1112 <sangminbag034@naver.com>
2024-10-04 17:54:32 +0900

    v0.6

--- week06/main.go
+++ week06/main.go
@@ -15,6 +15,7 @@ func main() {
 	//fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f) // 변환
 
 	fmt.Printf("%d * %f = %d\n", i, f, i*int(f))
-	fmt.Println(c1, c2)
-	fmt.Println(reflect.TypeOf(f), reflect.Type(i))
+	fmt.Println(c1, c2)    // 10진수 출력
+	fmt.Printf("%x\n", c2) // 유니코드 '김'에 대한 16진수 출력, home.unicode.org(유니코드 사이트)
+	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(c1), reflect.TypeOf(c2))
 }
