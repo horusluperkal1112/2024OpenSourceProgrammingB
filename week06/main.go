0b8bf467e54a8e1350f250e2d3bc7cfb9a3d0ad8
horusluperkal1112 <sangminbag034@naver.com>
2024-10-11 16:47:39 +0900

    v0.7 time

--- week06/main.go
+++ week06/main.go
@@ -6,16 +6,16 @@ import (
 )
 
 func main() {
-
 	i := 13
-	var f float64 = 12.9
+	var f float64 = 12.9 //f := 12.9
 	c1 := 'Z'
 	c2 := '김'
-	fmt.Printf("value i : %d, value f : %f\n", i, f)
-	//fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f) // 변환
 
-	fmt.Printf("%d * %f = %d\n", i, f, i*int(f))
+	fmt.Printf("value i : %d, value f : %f\n", i, f)
+	//fmt.Printf("%d * %f = %f\n", i, f, i*f) // invalid operation: i * f (mismatched types int and float64)
+	fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f) // conversion
+	//fmt.Printf("%d * %f = %d\n", i, f, i*int(f)) // conversion
 	fmt.Println(c1, c2)    // 10진수 출력
-	fmt.Printf("%x\n", c2) // 유니코드 '김'에 대한 16진수 출력, home.unicode.org(유니코드 사이트)
-	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(c1), reflect.TypeOf(c2))
+	fmt.Printf("%x\n", c2) // 유니코드 '김'에 대한 16진수 출력, UNICODE
+	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(i), reflect.TypeOf(c1), reflect.TypeOf(c2))
 }
