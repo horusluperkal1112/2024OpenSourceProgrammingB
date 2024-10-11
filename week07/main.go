4a166873d153e5d344441a7d926d4fe4b1421dd3
horusluperkal1112 <sangminbag034@naver.com>
2024-10-11 17:23:29 +0900

    v1.0 if

--- week07/main.go
+++ week07/main.go
@@ -3,13 +3,20 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 	fmt.Print("이름 입력 : ")
-	r := bufio.NewReader(os.Stdin)
-	name, err := r.ReadString('\n')
+	in := bufio.NewReader(os.Stdin)
+	name, err := in.ReadString('\n')
 	fmt.Println(name)
-	fmt.Println(err)
+
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println(name)
+	}
+
 }
