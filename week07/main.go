05d57de80f3f15a90afa402abf3a6d27bd32ed46
horusluperkal1112 <sangminbag034@naver.com>
2024-10-11 17:53:27 +0900

    1.1 Shadowing problem

--- week07/main.go
+++ week07/main.go
@@ -1,22 +1,11 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-)
-
 func main() {
-	fmt.Print("이름 입력 : ")
-	in := bufio.NewReader(os.Stdin)
-	name, err := in.ReadString('\n')
-	fmt.Println(name)
+	//Shadowing problem
 
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		fmt.Println(name)
-	}
-
+	//var float32 float32 = 9.2
+	//fmt.Println(float32)
+	//var number float32
+	//var fmt float32 = 9.1
+	//fmt.Println(float32)
 }
