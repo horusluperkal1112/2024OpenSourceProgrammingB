ba388ba9140dba4418c2e8a409f2aa67fd00e15f
horusluperkal1112 <sangminbag034@naver.com>
2024-10-25 17:19:26 +0900

    v1.9 Guess game

--- week09/main.go
+++ week09/main.go
@@ -14,10 +14,12 @@ import (
 func main() {
 	rand.Seed(time.Now().Unix()) //유닉스 시간 : 1970년 언젠가부터 지금의 초
 	answer := rand.Intn(6) + 1   // 주사위 1 ~ 6 사이의 수
-	fmt.Println(answer)
+	// fmt.Println(answer)
+
+	win := false
 
 	for guesses := 0; guesses < 3; guesses++ {
-		fmt.Printf("%d번의 기회가 남았습니다.", 3-guesses)
+		fmt.Printf("%d번의 기회가 남았습니다. 숫자 입력 : ", 3-guesses)
 		in := bufio.NewReader(os.Stdin)
 		input, err := in.ReadString('\n')
 
@@ -33,6 +35,7 @@ func main() {
 		fmt.Println(guess)
 
 		if answer == guess {
+			win = true
 			fmt.Println("정답입니다.")
 			break
 		} else if answer > guess {
@@ -41,5 +44,11 @@ func main() {
 			fmt.Println("입력하신 값은 정답보다 큽니다.")
 		}
 
+	}
+
+	if win {
+		fmt.Println("당신이 이겼습니다.")
+	} else {
+		fmt.Printf("당신이 졌습니다. 정답은 %d입니다\n", answer)
 	}
 }
