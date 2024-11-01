61f580f87838d5f34f30d278cf3e9f2c6442916f
horusluperkal1112 <sangminbag034@naver.com>
2024-11-01 18:26:45 +0900

    v3.0 Prime number

--- week10/main.go
+++ week10/main.go
@@ -4,14 +4,32 @@ import (
 	"bufio"
 	"fmt"
 	"log"
-	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
+func isPrime(n int) bool {
+
+	if n <= 1 {
+		return false
+	} else if n == 2 {
+		return true
+	} else if n%2 == 0 {
+		return false
+	} else {
+		i := 3
+		for i*i <= n {
+			if n%i == 0 {
+				return false
+			}
+			fmt.Printf("%d ", i)
+			i = i + 2
+		}
+	}
+	return true
+}
 func main() {
-	fmt.Printf("%f\n", math.Sqrt(25.0))
 	fmt.Print("정수 입력 : ")
 	in := bufio.NewReader(os.Stdin)
 	number, err := in.ReadString('\n')
@@ -25,26 +43,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var isPrime bool = true
-
-	if n <= 1 {
-		isPrime = false
-	} else if n == 2 {
-		isPrime = true
-	} else if n%2 == 0 {
-		isPrime = false
-	} else {
-		i := 3
-		for i*i <= n {
-			if n%i == 0 {
-				isPrime = false
-				break
-			}
-			fmt.Printf("%d ", i)
-			i = i + 2
-		}
-	}
-	if isPrime {
+	if isPrime(n) {
 		fmt.Printf("%d는(은) 소수입니다.", n)
 	} else {
 		fmt.Printf("%d는(은) 소수가 아닙니다.", n)
