2a136392cc9e698d743c453c7327f68222799329
horusluperkal1112 <sangminbag034@naver.com>
2024-11-25 08:26:41 +0900

    v#.#

--- week13/main.go
+++ week13/main.go
@@ -2,23 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"reflect"
 )
 
-func main() {
-	var emptySlice []bool
-	// emptySlice = make([]bool, 5)
-	fmt.Printf("%#v %d\n", emptySlice, len(emptySlice)) //slice zero value (nil)
-	if len(emptySlice) == 0 {
-		emptySlice = append(emptySlice, true)
-	}
-	fmt.Printf("%#v %d\n", emptySlice, len(emptySlice)) //[]bool(true), 1
+func test(i int, strs ...string) {
+	fmt.Println(i, strs, reflect.TypeOf(strs))
+}
 
-	gpas := [5]float32{3.5, 4.1, 4.5, 3.9, 4.23}
-	gpas_slice := gpas[1:4]
-	//gpas_slice[1] = 2.71
-	gpas[1] = 2.71
-	//gpas_slice = append(gpas_slice, 4.3)
-	gpas_slice = append(gpas_slice, 4.3, 5.55)
-	fmt.Println(len(gpas_slice), gpas_slice, gpas)
+func main() {
+	// fmt.Println(os.Args, len(os.Args), reflect.TypeOf(os.Args))
+	slices := os.Args[1:]
+	fmt.Println(slices, slices[1])
+	test(1, "123")
+	test(99, "123", "abc")
+	test(55)
+	test(0, "123", "abc", "123a")
 
 }
