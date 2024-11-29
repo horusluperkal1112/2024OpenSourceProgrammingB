33128f5a90609fb500e24636facbb174afd3b3b8
horusluperkal1112 <sangminbag034@naver.com>
2024-11-29 17:49:28 +0900

    V4.8 need to fix / struct

--- week14/main.go
+++ week14/main.go
@@ -2,25 +2,21 @@ package main
 
 import "fmt"
 
+type student struct {
+	id   int
+	name string
+	gpa  float64
+}
+
 func main() {
-	var student struct {
-		id   int
-		name string
-		gpa  float64
-	}
+	var student student
 
 	student.id = 202444064
 	student.name = "박상민"
 	student.gpa = 4.5
-
 	fmt.Println(student.gpa)
 
-	var student2 struct {
-		id   int
-		name string
-		gpa  float64
-	}
-
+	var student2 student
 	student2.id = 202444065
 	student2.name = "박상민2"
 	student2.gpa = 4.49
