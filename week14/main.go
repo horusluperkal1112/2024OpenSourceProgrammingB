deeacdff383361896a4de84a4994645894367ed5
horusluperkal1112 <sangminbag034@naver.com>
2024-11-29 18:24:15 +0900

    v4.n struct

--- week14/main.go
+++ week14/main.go
@@ -2,24 +2,41 @@ package main
 
 import "fmt"
 
-type student struct {
-	id   int
-	name string
-	gpa  float64
+// chile : 5000, adult : 10000, old man : 7000
+type visitor struct {
+	age  int
+	cost int
 }
 
+func totalCost(visitors []visitor) int {
+	total := 0
+	for _, v := range visitors {
+		total = total + v.cost
+	}
+	return total
+}
 func main() {
-	var student student
+	var numVisitor int
+	fmt.Print("How many people? : ")
+	fmt.Scanln(&numVisitor)
 
-	student.id = 202444064
-	student.name = "박상민"
-	student.gpa = 4.5
-	fmt.Println(student.gpa)
+	vs := make([]visitor, numVisitor)
+	for i := 0; i < numVisitor; i++ {
+		age := 0
+		fmt.Print("How old are you? : ")
+		fmt.Scanln(&age)
 
-	var student2 student
-	student2.id = 202444065
-	student2.name = "박상민2"
-	student2.gpa = 4.49
+		switch {
+		case age < 12:
+			vs[i] = visitor{age: age, cost: 5000}
 
-	fmt.Println(student2.gpa)
+		case age >= 12 && age < 65:
+			vs[i] = visitor{age: age, cost: 10000}
+
+		default:
+			vs[i] = visitor{age: age, cost: 7000}
+		}
+	}
+
+	fmt.Printf("Total price is %d won", totalCost(vs))
 }
